a7c14008be2f7f509c3d8c546f21128c6ea563f6
saket saurav <saketsingh6996@gmail.com>
2025-07-07 00:04:52 +0530

    Checking github commit

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -7,7 +7,7 @@ const Herosection = () => {
             YOUR FEET DESERVE THE BEST
 YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU
  WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE
-  HERE TO HELP YOU WITH OUR SHOES.
+  HERE TO HELP YOU WITH OUR SHOES
        </p>
   <div className="hero-btn">
     <button>shop now</button>
