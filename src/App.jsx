ebb2b9144d0cdb69ebd5719601a5129a24c097f3
saket saurav <saketsingh6996@gmail.com>
2025-07-06 23:49:19 +0530

    finisshed the designing and styling of the brand page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Navigation></Navigation>
       <Herosection></Herosection>
     </div>
-  )
-}
+  );
+};
 
 export default App
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,20 +1,31 @@
 const Herosection = () => {
     return (
-    <main className="hero"> 
+    <main className="hero container"> 
     <div className="hero-content">
         <h1>YOUR FEET DESERVES THE BEST</h1>
-        <P>
+        <p>
             YOUR FEET DESERVE THE BEST
 YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU
  WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE
   HERE TO HELP YOU WITH OUR SHOES.
-       </P>
+       </p>
   <div className="hero-btn">
     <button>shop now</button>
-    <button>category</button>
+    <button 
+    secondary-btn>category</button>
+  </div>
+  <div className="shopping">
+    <p>Also Available on</p>
+
+    <div className="brand-icon">
+      <img src="/images/amazon.png" alt="amazon-logo"/>
+      <img src="/images/flipkart.png" alt="flipkart-logo"/>
+    </div>
   </div>
     </div>
-    <div className="hero-image"></div>
+    <div className="hero-image">
+      <img src="/images/shoe_image.png" alt="hero-image" />
+    </div>
     </main>
     );
 };
